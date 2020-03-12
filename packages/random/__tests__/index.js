f7dca52ef831faa02f105c3079ade919925cad30
greg <greg.hearn@icloud.com>
2020-03-12 13:20:59 +1000

    Commit implemented test cases

---
+++ packages/random/__tests__/index.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const random = require('../index')
+const MIN_NUMBER = 0
+const MAX_NUMBER = Number.MAX_SAFE_INTEGER
+
+describe('when random numbers are required...', () => {
+  beforeEach(() => {
+  })
+  afterEach(() => {
+  })
+  test('a number is generated successfully', () => {
+    const num = random.number()
+    expect(num).toBeGreaterThanOrEqual(MIN_NUMBER)
+    expect(num).toBeLessThanOrEqual(MAX_NUMBER)
+  })
+  test('a number is less than a specified number', () => {
+    expect(random.number(0, 100)).toBeLessThanOrEqual(100)
+  })
+  test('a number is greater than a specified number', () => {
+    expect(random.number(0, 100)).toBeGreaterThanOrEqual(0)
+  })
+  test('a number is between a specified set of numbers', () => {
+    const num = random.number(0, 100)
+    expect(num).toBeGreaterThanOrEqual(0)
+    expect(num).toBeLessThanOrEqual(100)
+  })
+})
+
+describe('when random strings are required...', () => {
+  beforeEach(() => {
+  })
+  afterEach(() => {
+  })
+  test('a string is generated successfully', () => {
+    const str = random.string()
+    expect(str).not.toBeNull()
+    expect(str.length).toBeGreaterThan(0)
+  })
+  test('a string is generated successfully if the parameter is not a number', () => {
+    const str = random.string('abc123')
+    expect(str).not.toBeNull()
+    expect(str.length).toBeGreaterThan(0)
+  })
+  test('a string is generated successfully if the parameter is greater than ' +
+    'the limit of 255 characters in length', () => {
+    const str = random.string(Number.MAX_SAFE_INTEGER)
+    expect(str).not.toBeNull()
+    expect(str.length).toEqual(255)
+  })
+  test('a string is generated successfully if the parameter is greater than ' +
+    'the limit of 255 characters in length by 1', () => {
+    const str = random.string(256)
+    expect(str).not.toBeNull()
+    expect(str.length).toEqual(255)
+  })
+  test('a string has a length equal to the specified number parameter', () => {
+    const len = 8
+    const str = random.string(len)
+    expect(str).not.toBeNull()
+    expect(str.length).toEqual(len)
+  })
+})
--- packages/random/index.js
+++ packages/random/index.js
@@ -6,34 +6,18 @@ const normalizeNumber = (n) => {
   if (n < 0) { n = 0 }
   return n
 }
+const DEFAULT_RANDOM_STRING_LENGTH = 16
+const MAX_RANDOM_STRING_LENGTH = 255
 const CHAR_LOWER = 'abcdefghijklmnopqrstuvwxyz'
 const CHAR_UPPER = CHAR_LOWER.toUpperCase()
 const NUMBER = '0123456789'
 const DATA_FOR_RANDOM_STRING = CHAR_LOWER + CHAR_UPPER + NUMBER
 
 class Random {
-  constructor (options = {}) {
-    this.options = options
-  }
-
-  /**
-   * Returns a non-negative random integer.
-   */
-  /* next () {
-    const max = normalizeNumber(Number.MAX_SAFE_INTEGER)
-    return Math.floor(Math.random() * max)
-  } */
-
-  /**
-   * Returns a non-negative random number that is less than the supplied maximum.
-   */
-  /* nextFrom (max = Number.MAX_SAFE_INTEGER) {
-    max = normalizeNumber(max)
-    return Math.floor(Math.random() * max)
-  } */
-
   /**
    * Returns a random number that is within a supplied range.
+   * @param {*} min
+   * @param {*} max
    */
   number (min = 0, max = Number.MAX_SAFE_INTEGER) {
     min = normalizeNumber(min)
@@ -41,7 +25,13 @@ class Random {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
+  /**
+   * Returns a random string that is equal to the supplied length.
+   * @param {*} length
+   */
   string (length = 10) {
+    if (typeof length !== 'number') { length = DEFAULT_RANDOM_STRING_LENGTH }
+    if (length > MAX_RANDOM_STRING_LENGTH) { length = MAX_RANDOM_STRING_LENGTH }
     let ret = ''
     for (let index = 0; index < length; index += 1) {
       ret += DATA_FOR_RANDOM_STRING.charAt(
@@ -52,6 +42,4 @@ class Random {
   }
 }
 
-module.exports = {
-  Random
-}
+module.exports = new Random()
