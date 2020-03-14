b4dbe566bbfb590a5d9577faab1eaaff1990cb9f
greg <greg.hearn@icloud.com>
2020-03-14 16:34:33 +1000

    moved to single args parameter initalization for middleware

--- packages/squareup-authenticity-middyware/index.js
+++ packages/squareup-authenticity-middyware/index.js
@@ -7,12 +7,17 @@ const validate = require('./validate')
  *
  * @param {*} args
  */
-const squareupAuthenticityMiddyware = (signatureKey) => {
+const squareupAuthenticityMiddyware = (args) => {
+  const defaults = {
+    signatureKey: ''
+  }
+  const options = Object.assign({}, defaults, args)
+
   return ({
     before: (handler, next) => {
       const { event } = handler
       if (event) {
-        if (!validate.authenticity(event, signatureKey)) {
+        if (!validate.authenticity(event, options.signatureKey)) {
           throw new Error(
             `Could not validate the authenticity of this message ${JSON.stringify(event)}`
           )
