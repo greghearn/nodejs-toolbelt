490e61fa4a4875a9eddf00dcfb227c6707eefc49
greg <greg.hearn@icloud.com>
2020-04-02 19:13:42 +1000

    patch sns capture functionality

--- packages/accept-message-middyware/event-source/sns.js
+++ packages/accept-message-middyware/event-source/sns.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const ajv = require('../lib/ajv')
+const util = require('../lib/util')
+
 const keyToMatch = 'eventsource'
 const valueToMatch = 'aws:sns'
 
@@ -8,6 +11,19 @@ const match = function match (event) {
     const { Records = [] } = event
     var index = -1
 
+    if (Records.length > 0) {
+      index = Object.entries(Records[0]).findIndex(([key, value]) => {
+        return (
+          (key.toLowerCase() === keyToMatch) &&
+          (value.toLowerCase() === valueToMatch)
+        )
+      })
+    }
+    /*
+     * removed bulk record checking as we dont have any example data
+     * where more than 1 Record element exists yet.
+     * code has been left in for a possible use later on
+     *
     for (var i = 0, len = Records.length; i < len; i += 1) {
       index = Object.entries(Records[i]).findIndex(([key, value]) => {
         return (
@@ -19,6 +35,7 @@ const match = function match (event) {
         break
       }
     }
+    */
     return index >= 0
   } catch {
     return false
@@ -26,6 +43,14 @@ const match = function match (event) {
 }
 
 const capture = function capture (event, options) {
+  const { schema = {} } = options
+  const { Records = [] } = event
+  const { Message = {} } = Records[0].Sns
+
+  event.Records[0].Sns.Message = util.json.parse(Message)
+  ajv.test(schema, event)
+
+  if (!options.convert) event.Records[0].Sns.Message = Message
 }
 
 module.exports = {
